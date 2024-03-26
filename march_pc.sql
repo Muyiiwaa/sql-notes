ab6d7a56456dbee0cd6404b80f6f1d72bac4cdc7
Muyiwa <forevermuyii@gmail.com>
2024-03-26 11:01:07 +0100

    included exercise

--- march_pc.sql
+++ march_pc.sql
@@ -31,6 +31,42 @@ WHERE Freight < 50 and ShipCountry = 'Mexico';
 
 -- return the names of female employees in our companies
 
+select firstName, lastname, TitleOfCourtesy
+from employees
+WHERE TitleOfCourtesy = 'Ms.' OR TitleOfCourtesy = 'Mrs.';
+
+-- method 2
+
+select firstname, lastname, TitleOfCourtesy
+from employees
+WHERE TitleOfCourtesy IN ('Mrs.','Ms.');
+
+-- return the names of customers that are not based in US
+
+SELECT companyname, contactname, country
+FROM customers
+WHERE not country = 'USA';
+
+-- handling date ranges
+
+-- return the orderid, orderdate, city and country of all orders sent to canada
+-- in the first quarter of 1997
+
+select orderid, orderdate, shipcity, shipCountry
+FROM orders
+where (ShipCountry = 'Canada') AND
+    (orderDate BETWEEN '1997-01-01' and '1997-03-31');
+
+-- EXERCISE 
+
+-- 1. RETURN THE NAMES OF PRODUCTS COSTING MORE THAN 20 DOLLARS THAT WE HAVE 
+-- IN STOCK
+-- 2. RETURN THE NAMES OF CUSTOMERS IN ALL UK CITIES EXCEPT LONDON
+-- 3. RETURN THE LIST OF US ORDERS SENT IN DECEMBER 1997
+
+
+
+
 
 
 
